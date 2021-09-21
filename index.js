7ed58f15c7e86ccd9d0f0b79961cbaf4b90d6b1f
max anselmo <itmanselmo@gmail.com>
2021-09-21 18:20:12 -0400

    completed assignment according to the test suite, found an item in an array with the find method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+function isWin(element, index, array) {
+    return (element.result === 'W');
+}
+
+function superbowlWin(record) {
+    let winningYear = record.find(isWin);
+    if(winningYear != undefined){
+        let year = winningYear.year;
+        console.log(year);
+        return year;
+    }
+    else {
+        console.log(winningYear);
+        return undefined;
+    }
+}
